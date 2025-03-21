d08a5564cf27fab742cd2faa4d455256a18cb00c
Maksym Sabov <maksim50.97@gmail.com>
2025-03-21 22:35:34 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-center mt-10">
+      Hello, Construction Escrow MVP!
+    </div>
+  );
+}
+
+export default App;
